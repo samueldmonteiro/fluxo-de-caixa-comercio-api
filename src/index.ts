aebdb5ae0c0b5d4b42d7ce515d6e819b50302568
Samuel D. Monteiro <samuel.dvmonteiro@gmail.com>
2025-12-07 13:01:35 -0300

    criando vercel.json

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import app from './server';
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`🚀 API rodando firme na porta ${PORT}`);
-  console.log(`🔗 http://localhost:${PORT}/api`);
-  console.log(`📚 Docs: http://localhost:${PORT}/docs`);
-});
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,17 @@
 import 'dotenv/config';
 import 'reflect-metadata';
 import app from './app';
-import { setupSwagger } from './config/swagger';
 
+const PORT = process.env.PORT || 3000;
+
+
+if (process.env.VERCEL_ENV != 'production') {
+  app.listen(PORT, () => {
+    console.log(`🚀 API rodando firme na porta ${PORT}`);
+    console.log(`🔗 API disponível em: http://localhost:${PORT}/api`);
+    console.log('Docs available on http://localhost:3000/docs');
+  });
+
+}
 
 export default app;
